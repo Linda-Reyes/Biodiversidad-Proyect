3e32504fa6f3df2c7f309e8dbd97a721df06d774
Linda Reyes <lindamreyesr@gmail.com>
2024-08-06 01:49:27 -0500

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css"; // Importar estilos de Leaflet
 import L from "leaflet"; // Importar Leaflet para solucionar problema de iconos
 
-
 function App() {
   const [menu, setMenu] = useState(false);
   const submitShowMenu = () => {
     setMenu(!menu);
   };
 
+  const zoom = 9;
+  const Sumapaz = [4.41667, -74.1];
+  const Chicaque = [4.60634279, -74.30475616];
+  const tunos = [4.6161871, -74.35184479];
+  const monarca = [4.85580111, -74.09506989];
+  const flores = [4.72426987, -74.25228119];
+  const Oso = [4.8295269, -47.0136795];
+  const vega = [5.00206041, -74.33998871];
+
   return (
-    <>
-      <div>
-        <header>
-           <div className="fixed text-[#BFEA7C] h-[64px] bg-[#416D19] flex justify-between w-full items-center px-4 text-normal font-semibold">
+    <div className='font-["Julius Sans One"] font-light'>
+      <header>
+        <div className="fixed text-[#BFEA7C] h-[64px] bg-[#416D19] flex justify-between w-full items-center px-4 text-normal font-semibold">
           <ul className="gap-3 ">
             <li className="text-xl cursor-pointer">BioCundi</li>
           </ul>
@@ -63,39 +70,37 @@ function App() {
             </div>
           </section>
         </div>
-        </header>
+      </header>
+      <section className="bg-[url('/bird.svg')] h-[500px] min-w-screen bg-center bg-cover pt-10">
+        <div className="pt-7">
+          <h1 className='font-["Julius Sans One"] font-light  text-[#FFF67E] sm:text-4xl whitespace-break-spaces text-xl w-[250px] pl-6]'>
+            Senderos y Biodiversidad CundiBoyacense
+          </h1>
+        </div>
+        <div id="sobrenosotros" className="h-[600px]"></div>
+      </section>
 
-        <section className="bg-[url('/bird.svg')] h-[500px] min-w-screen bg-center bg-cover pt-10">
-          <div className="pt-7">
-            <h1 className='font-["Julius Sans One"] font-light  text-[#FFF67E] sm:text-4xl whitespace-break-spaces text-xl w-[250px] pl-6]'>
-              Senderos y Biodiversidad CundiBoyacense
-            </h1>
-          </div>
-          <div id="sobrenosotros" className="h-[600px]"></div>
-        </section>
-
-        <section>
-          <article className="text-white bg-gray-600 ">
-            <div className="gap-3 p-6 sm:flex">
-              <div className=" sm:grid sm:mt-9 ">
-                <h2 className="text-2xl text-[#FFF67E] sm:h-[50px]">
-                  Quienes somos
-                </h2>
-                <div className="sm:w-[400px]">
-                  <p className="font-light text-[#BFEA7C] ">
-                    Descubre la riqueza única de la biodiversidad en Cundinamarca
-                    y Boyacá. Esta región alberga especies únicas en el mundo y
-                    varias endémicas, convirtiéndose en un tesoro natural
-                    invaluable. Acompáñanos en un viaje por senderos accesibles
-                    para conocer de cerca la flora, fauna y ecosistemas que hacen
-                    de este lugar un paraíso biodiverso. ¡Explora, aprende y
-                    celebra la naturaleza con nosotros!
-                  </p>
-                </div>
+      <section>
+        <article className="text-white bg-gray-600 ">
+          <div className="gap-3 p-6 sm:flex">
+            <div className=" sm:grid sm:mt-9 ">
+              <h2 className="text-2xl text-[#FFF67E] sm:h-[50px]">
+                Quienes somos
+              </h2>
+              <div className="sm:w-[400px]">
+                <p className="font-light text-[#BFEA7C] ">
+                  Descubre la riqueza única de la biodiversidad en Cundinamarca
+                  y Boyacá. Esta región alberga especies únicas en el mundo y
+                  varias endémicas, convirtiéndose en un tesoro natural
+                  invaluable. Acompáñanos en un viaje por senderos accesibles
+                  para conocer de cerca la flora, fauna y ecosistemas que hacen
+                  de este lugar un paraíso biodiverso. ¡Explora, aprende y
+                  celebra la naturaleza con nosotros!
+                </p>
               </div>
+            </div>
 
-
-               <div id="senderos" className="p-6">
+            <div id="senderos" className="p-6">
               <img className="sm:h-[auto] w-[auto]" src="/imagen1.jpg" alt="" />
             </div>
           </div>
@@ -287,12 +292,44 @@ function App() {
           </section>
         </div>
       </section>
-      
+      <footer className="bg-[black]  text-center">
+        <div className="flex justify-center gap-3 text-[#faf6c2] text-lg font-light ">
+          <a
+            className="cursor-pointer hover:text-white hover:-translate-y-1 transition hover:duration-200"
+            href="https://github.com/carlos92aandres"
+            target="_blank"
+          >
+            Carlos Garcia
+          </a>
 
-      </div>
-
-    </>
-  )
+          <a
+            className="cursor-pointer hover:text-white hover:-translate-y-1 transition hover:duration-200"
+            href="https://github.com/Linda-Reyes"
+            target="_blank"
+          >
+            Linda Reyes
+          </a>
+          <a
+            className="cursor-pointer hover:text-white hover:-translate-y-1 transition hover:duration-200"
+            href="https://github.com/Dianisvibra5"
+            target="_blank"
+          >
+            Diana Diaz
+          </a>
+          <a
+            className="cursor-pointer hover:text-white hover:-translate-y-1 transition hover:duration-200"
+            href="https://github.com/jcesarrios"
+            target="_blank"
+          >
+            Julio Rios
+          </a>
+        </div>
+        <h3 className="text-xl font-light text-[#FFF67E]">
+          © Copyright todos los derechos reservados
+        </h3>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
